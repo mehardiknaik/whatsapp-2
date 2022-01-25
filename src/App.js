9cd9f195554ab6162deeba020ea9c72cebadb81c
Hardik Naik <hardiknaik7444@gmail.com>
2022-01-25 21:19:49 +0530

    toast msg

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Main from "./pages/Main";
-
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
     <>
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,19 @@
 import { Container } from "@mui/material";
 import React, { useState } from "react";
+import { toast } from "react-toastify";
 import styled from "styled-components";
 import Message from "../components/Message";
 import RecentTable from "../components/RecentTable";
-
+toast.configure({
+  position: "top-center",
+  autoClose: 5000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+  icon: "✅",
+});
 const MainContainer = styled.div`
   display: flex;
   justify-content: center;
@@ -26,24 +36,27 @@ const Main = () => {
 
     var receiveddata = JSON.stringify(newdata);
     localStorage.setItem("recent", receiveddata);
-
     setrecent(JSON.parse(localStorage.getItem("recent")));
   };
 
   const cleanStorage = () => {
     localStorage.clear();
     setrecent(JSON.parse(localStorage.getItem("recent")));
+    toast("All Numbers are Deleted");
   };
 
   const deleteRow = (index) => {
     var myArray = recent;
     var removedObject = myArray.splice(index, 1);
+    console.log('removedObject :',  JSON.stringify(removedObject));
+
     if (myArray.length <= 0) {
       cleanStorage();
     } else {
       var receiveddata = JSON.stringify(myArray);
       localStorage.setItem("recent", receiveddata);
       setrecent(JSON.parse(localStorage.getItem("recent")));
+      toast(`${removedObject[0].number} are Deleted`);
     }
   };
 
