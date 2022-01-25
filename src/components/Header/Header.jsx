09967ed9637569fef67021dcedb7ad96ce0b13ef
Hardik Naik <hardiknaik7444@gmail.com>
2022-01-25 21:04:30 +0530

    toasr

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,13 +2,13 @@ import logo from "./logo.png";
 import styles from "./Header.module.css";
 
 const Header = () => {
-  const Text = ["W", "h","a","t","s","A","p","p","-","2"];
+  const Text = "WhatsApp-2";
   return (
     <div className={styles.Container}>
       <img src={logo} width={35} height={35} alt="" />
       <div className={styles.text}>
-        {Text.map((txt) => (
-          <li>
+        {[...Text].map((txt,index) => (
+          <li key={index}>
             <input type="checkbox" />
             <div>{txt}</div>
           </li>
